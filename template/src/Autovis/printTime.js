bb446905e6b46a215a65aaf40e11d878c379a0ba
Adam Marciniak <adam668@gmail.com>
2019-12-29 13:53:09 -0800

    updated printTime.js to work now
    compiles and runs

--- template/src/Autovis/printTime.js
+++ template/src/Autovis/printTime.js
@@ -1,6 +1,6 @@
 import pad from './pad';
 
-export default = (currentTime) => {
+export default (currentTime) => {
   const minutes = Math.floor(currentTime / 60);
   const seconds = Math.floor(currentTime % 60);
   return `${pad(minutes, 2)}:${pad(seconds, 2)}`;
