52bd84d3a210009f7ade49a42f19f9962168e87c
Sam Watkinson <sam.ws.watkinson@gmail.com>
2019-12-29 22:52:40 -0800

    comment

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -1,3 +1,7 @@
+// Please note: Basic configuration used to be done in this file, but is no longer!
+// Use StudioTemplate.tsx for pre-made song visualizations you intend to render and save,
+// or use LiveTemplate.tsx for real-time audio input.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import StudioTemplate from './StudioTemplate';
