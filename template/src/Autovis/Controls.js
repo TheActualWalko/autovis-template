d28eba0523306577a12ab7c9d3d818f78cae6f86
Adam Marciniak <adam668@gmail.com>
2019-12-30 19:33:11 -0800

    Revert "Added drag behavior. Really janky"
    This reverts commit aff06e975f886f837c0e47a6a028b40127822fbe.

--- template/src/Autovis/Controls.js
+++ template/src/Autovis/Controls.js
@@ -1,91 +1,44 @@
-import React, {useState,useEffect} from 'react';
+import React from 'react';
 import './controls.css';
 import toPercent from './toPercent';
 import printTime from './printTime';
 
-const getXPositionRatio = (xPos, element) => {
-  const rect = element.getBoundingClientRect();
-  const mouseXRel = xPos - rect.x;
+const getEventXPositionRatio = (e) => {
+  const rect = e.currentTarget.getBoundingClientRect();
+  const mouseXRel = e.clientX - rect.x;
   const positionRatio = Math.min(Math.max(0, mouseXRel / rect.width), 1);
   return positionRatio;
 };
 
+export default ({ onPlay, onPause, paused, onSeek, currentTime, duration }) => (
+  <div className="controls">
+    <button
+      className="play"
+      onClick={() => {
         paused ? onPlay() : onPause();
-export const useMousePosition = () => {
-  const [draggingObject, setDraggingObject] = useState({dragging:false, x: 0,y:0});
-  useEffect(() => {
-    const setMouseDown = (e) => {
-      console.log(e.target.className);
-      if (e.target.className === 'progressLine' ||
-      e.target.className === 'timeline' ||
-      e.target.className === 'progressSlider' ||
-      e.target.className === 'progressSliderWrapper' ) {
-        setDraggingObject({...draggingObject, dragging: true});
-        console.log(draggingObject);
-      }
-    }
-
-    const setMouseMove = (e) => {
-      if (draggingObject.dragging === true) {
-        setDraggingObject({...draggingObject, x: e.clientX});
-      }
-    }
-
-    const setMouseUp = (e) => {
-        setDraggingObject({...draggingObject, dragging: false});
-    }
-    window.addEventListener("mousedown", setMouseDown);
-    window.addEventListener("mouseup", setMouseUp);
-    window.addEventListener("mousemove", setMouseMove);
-
-    return () => {
-      window.removeEventListener("mousedown", setMouseDown);
-      window.removeEventListener("mouseup", setMouseUp);
-      window.removeEventListener("mousemove", setMouseMove);
-    };
-  }, [draggingObject]);
-
-  return draggingObject;
-};
-
-
-export default ({ onPlay, onPause, paused, onSeek, currentTime, duration }) => 
-  {
-    const dragging = useMousePosition();
-    useEffect(() => {
-      const sliderElement = document.querySelector('.timeline');
-      onSeek(getXPositionRatio(dragging.x,sliderElement) * duration)
-    },[dragging]);
-
-    return (
-    <div className="controls">
-      <button
-        className="play"
-        style={{userSelect:'none'}}
-        onClick={() => {
-          console.log(paused);
-          paused ? onPlay() : onPause();
-        }}
-      >
-        {paused ? 'Play' : 'Pause'}
-      </button>
-      <div
-        className="timeline"
-      >
-        <div className="progressLine">
-          <div
-            className="progressSliderWrapper"
-            style={{
-              width: toPercent(currentTime/duration)
-            }}
-          >
-            <div className="progressSlider"></div>
-          </div>
+      }}
+    >
+      {paused ? 'Play' : 'Pause'}
+    </button>
+    <div
+      className="timeline"
+      onClick={event => {
+        onSeek(getEventXPositionRatio(event) * duration);
+      }}
+    >
+      <div className="progressLine">
+        <div
+          className="progressSliderWrapper"
+          style={{
+            width: toPercent(currentTime/duration)
+          }}
+        >
+          <div className="progressSlider"></div>
         </div>
       </div>
-      <h4 className="timeReadout">
-        {`${printTime(currentTime)} / ${printTime(duration)}`}
-      </h4>
     </div>
-  )
-};
+    <h4 className="timeReadout">
+      {`${printTime(currentTime)} / ${printTime(duration)}`}
+    </h4>
+  </div>
+);
--- template/src/Autovis/StudioRendererInterface.tsx
+++ template/src/Autovis/StudioRendererInterface.tsx
@@ -122,7 +122,6 @@ export default ({
     <div style={{textAlign: 'center'}}>
       {renderingVideo ? null : (
         <>
-          
           <div style={{ position: 'fixed', top: 0, right: 0, width: 'auto', textAlign: 'right', zIndex: 3 }}>
             <button onClick={() => window.location.href = `${window.location.protocol}//${window.location.host}?live=true`}>
               Switch to Live Input Setup
